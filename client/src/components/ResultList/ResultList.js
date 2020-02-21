ef27786fdaa7cee496ec6ba9a3a43576369c461e
Jeff Huang <jeffshuang2@gmail.com>
2020-02-21 12:01:58 -0800

    Added a join method sepearated by commas to books that have multiple authors.

--- client/src/components/ResultList/ResultList.js
+++ client/src/components/ResultList/ResultList.js
@@ -13,7 +13,7 @@ class ResultList extends Component {
             key={book.id}
             title={book.volumeInfo.title}
             link={book.volumeInfo.previewLink}
-            authors={book.volumeInfo.authors}
+            authors={book.volumeInfo.authors.length > 1 ? book.volumeInfo.authors.join(", ") : book.volumeInfo.authors}
             image={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : "https://previews.123rf.com/images/pavelstasevich/pavelstasevich1811/pavelstasevich181101065/112815953-no-image-available-icon-flat-vector.jpg"}
             description={book.volumeInfo.description}
             saveGoogleBook={this.props.saveGoogleBook}
