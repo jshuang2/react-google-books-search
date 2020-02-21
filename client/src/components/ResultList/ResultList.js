5dbeb12a6219bcc08b046f2400ec9029f9617a44
Jeff Huang <jeffshuang2@gmail.com>
2020-02-21 11:41:26 -0800

    Added a default image if there isn't an image given from Google Books. Added max-width restrictions for the image.

--- client/src/components/ResultList/ResultList.js
+++ client/src/components/ResultList/ResultList.js
@@ -14,7 +14,7 @@ class ResultList extends Component {
             title={book.volumeInfo.title}
             link={book.volumeInfo.previewLink}
             authors={book.volumeInfo.authors}
-            image={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : <h5>No image to display</h5>}
+            image={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : "https://previews.123rf.com/images/pavelstasevich/pavelstasevich1811/pavelstasevich181101065/112815953-no-image-available-icon-flat-vector.jpg"}
             description={book.volumeInfo.description}
             saveGoogleBook={this.props.saveGoogleBook}
             />
--- client/src/components/ResultListItem/ResultListItem.js
+++ client/src/components/ResultListItem/ResultListItem.js
@@ -7,12 +7,12 @@ function ResultListItem(props) {
             <div className="card">
                 <h5 className="card-header">Featured</h5>
                 <div className="card-body">
-                    <img src={props.image} />
+                    <img src={props.image} style={{maxWidth: "100px"}}/>
                     <h5 className="card-title">{props.title}</h5>
                     <p>{props.id}</p>
                     <p className="card-text" >{props.description}</p>
                     <p>{props.authors}</p>
-                    <a href={props.link} className="btn btn-primary">View Book</a>
+                    <a href={props.link} target="_blank" className="btn btn-primary">View Book</a>
                     <button onClick={() => props.saveGoogleBook}>Save</button>
                 </div>
             </div>
