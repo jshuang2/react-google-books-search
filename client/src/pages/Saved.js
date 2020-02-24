6133e17c7211e6584631a87ef7dc84a742e50b24
Jeff Huang <jeffshuang2@gmail.com>
2020-02-24 15:24:57 -0800

    Saved books page grabs saved books from DB and sets state locally.

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -2,8 +2,28 @@ import React, { Component } from 'react'
 import { Col, Row, Container } from "../components/Grid/Grid";
 import Nav from "../components/Nav/Nav";
 import Jumbotron from "../components/Jumbotron/Jumbotron";
+import API from '../utils/API';
 
 class Saved extends Component {
+
+    state = {
+        savedBooks: []
+    }
+
+    componentDidMount = () => {
+        API.getBooks()
+        .then(res => {
+            this.setState({
+                savedBooks: res.data
+            })
+            console.log("This is the res from getBooks", res);
+        })
+        .catch(err => {
+            console.log("This is the error", err);
+        })
+    }
+
+
     render() {
         return (
             <div>
