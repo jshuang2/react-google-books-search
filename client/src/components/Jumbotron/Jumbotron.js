cbc1da4d5d9f31a66e6ab4ee25d0c454c35d2fd2
Jeff Huang <jeffshuang2@gmail.com>
2020-02-26 00:07:52 -0800

    More style changes. Added picture as jumbotron background.

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -5,9 +5,9 @@ function Jumbotron() {
     return (
         <div>
             <div className="jumbotron">
-                <h1 className="display-4 text-center">(React) Google Books Search</h1>
+                <h1 className="display-4 text-center myText">Google Books Search</h1>
                 <br></br>
-                <h2 className="text-center">Search for and save books of interest</h2>
+                <h2 className="text-center myText">Search for and save books of interest. Built with React.</h2>
             </div>
         </div>
     )
